89d86dfee724b9b667d76f6bb84a608b03e8e6f8
Elen Caroline <carolineelen98@gmail.com>
2020-08-11 16:39:27 -0300

    parametro de alerta nos campos realizado

--- script.js
+++ script.js
@@ -1,95 +1,74 @@
-/*function cadFunc(itemform){
+document.getElementById('submit').addEventListener('click', function () {
+    if (document.querySelectorAll(':invalid').length > 0) {
+        alert("CAMPOS FALTANDO")
+    }
+})
 
-    if (itemform.nomeFun.value == ""){
-        alert("Digite seu nome")
-        return false
-    }
-    if(itemform.dataNasc.value == ""){
-        alert("Digite seu CPF")
-        return false
-    }
-    if(itemform.est_civil.value == ""){
-        alert("Estado Cívil Inválido")
-    }
-    return true
-}*/
-/*
-const nome = document.getElementById('nomeFun')
-const nascimento = document.getElementById('dataNasc')
-const btn = document.getElementById('submit')
+function validForm(element) {
+    var nome = document.forms["work_us"]["nomeFun"].value
+    var nascimento = document.forms["work_us"]["dataNasc"].value
+    var estCivil = document.forms["work_us"]["est_civil"].value
+    var cpf = document.forms["work_us"]["cpf"].value
+    var rg = document.forms["work_us"]["rg"].value
+    var cnh = document.forms["work_us"]["cnh-tipo"].value
+    var cep = document.forms["work_us"]["cep"].value
+    var rua = document.forms["work_us"]["rua"].value
+    var num = document.forms["work_us"]["numero"].value
+    var compl = document.forms["work_us"]["complement"].value
+    var bairro = document.forms["work_us"]["bairro"].value
+    var city = document.forms["work_us"]["city"].value
+    var uf = document.forms["work_us"]["estado"].value
 
-btn.addEventListener('click', function(){
-    if(nome.checked && !nascimento.checked){
-        alert('Data Nascimento Invalido')
-        return
-    }
-    
-})*/
 
-function validForm(){
-    var nome  = document.forms["work_us"]["nomeFun"].value
-    var nascimento  = document.forms["work_us"]["dataNasc"].value
-    var estCivil  = document.forms["work_us"]["est_civil"].value
-    var cpf  = document.forms["work_us"]["cpf"].value
-    var rg  = document.forms["work_us"]["rg"].value
-    var cnh  = document.forms["work_us"]["cnh-tipo"].value
-    var cep  = document.forms["work_us"]["cep"].value
-    var rua  = document.forms["work_us"]["rua"].value
-    var num  = document.forms["work_us"]["numero"].value
-    var compl  = document.forms["work_us"]["complement"].value
-    var bairro  = document.forms["work_us"]["bairro"].value
-    var city  = document.forms["work_us"]["city"].value
-    var uf  = document.forms["work_us"]["estado"].value
-
-    if(nome == ""){
-        alert('Nome Inválido')
+    if (nome == "") {
+        alert('Nome inválido')
         return false
     }
-    if(nascimento == "" || nascimento.length < 9){
-        alert('Data de Nascimento Inválido ou Incompleto!')
+    if (nascimento == "" || nascimento.length < 10) {
+        alert('Data de nascimento inválido ou incompleto')
         return false
     }
-    if(estCivil == ""){
-        alert ('Estado Cívil Inválido')
+    if (estCivil.selectedIndex = 0) {
+        alert('Estado Cívil Inválido')
         return false
     }
-    if(cpf == "" || cpf.length < 13){
+    if (cpf == "" || cpf.length < 14) {
         alert('CPF inválido ou incompleto')
         return false
     }
-    if(rg == "" || rg.length < 11){
+    if (rg == "" || rg.length < 12) {
         alert("RG inválido ou incompleto")
         return false
     }
-    if(cnh == "" || cnh.length != 1 ){
-        alert('CNH inválido ou incompleto')
+    if (cnh == "") {
+        alert('CNH inválido')
         return false
     }
-    if(cep == "" || cep.length < 9){
+    if (cep == "" || cep.length < 10) {
         alert('CEP inválido ou incompleto')
         return false
     }
-    if(rua == ""){
+    if (rua == "") {
         alert('Rua inválida')
         return false
     }
-    if(num == "" || num.length < 2){
+    if (num == "" || num.length < 2) {
         alert('Número inválido ou incompleto')
         return false
     }
-    if(compl == ""){
+    if (compl == "") {
         alert('Complemento inválido')
         return false
     }
-    if(bairro == ""){
+    if (bairro == "") {
         alert('Bairro inválido')
         return false
     }
-    if(city == ""){
+    if (city == "") {
         alert('Cidade inválida')
         return false
     }
-    if(uf == ""){
+    if (uf.selectedIndex = 0) {
         alert('Estado inválido')
         return false
     }
